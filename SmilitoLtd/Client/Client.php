6e4abf2101bb73d784c327c4d241309ed4affb35
Tom Wright <contact@tomwright.me>
2024-08-26 21:09:37 +0100

    Fix a bug in Client login response mapping

--- SmilitoLtd/Client/Client.php
+++ SmilitoLtd/Client/Client.php
@@ -33,15 +33,27 @@ class Client
 
         $res = json_decode($result, true);
 
+        $settings = [];
+        if (array_key_exists('settings', $res)) {
+            $settings = $res['settings'];
+        }
+
         $resp = new LoginResponse();
-        $resp->jwt = $res['jwt'];
-        $resp->settings->claimableRewardsEmailEnabled = $res['claimable_rewards_email_enabled'];
+        if (array_key_exists('jwt', $res)) {
+            $resp->jwt = $res['jwt'];
+        }
+        if (array_key_exists('claimableRewardsEmailEnabled', $settings)) {
+            $resp->settings->claimableRewardsEmailEnabled = $settings['claimableRewardsEmailEnabled'];
+        }
 
         $this->jwt = $resp->jwt;
 
         return $resp;
     }
 
+    /**
+     * @throws \Exception
+     */
     public function sendClaimableRewardEmail($email, $name, $basketId, $basketValue)
     {
         $ch = curl_init($this->baseUrl . '/serve/v1/send-claimable-rewards-email');
@@ -54,14 +66,14 @@ class Client
 
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'authorization:Bearer ' . $this->jwt));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         $result = curl_exec($ch);
         curl_close($ch);
 
         if ($result === false) {
-            error_log('Failed to send Smilito claimable rewards via email');
+            throw new \Exception('Failed to send ClaimableRewardEmail...' . curl_error($ch));
         }
     }
 
